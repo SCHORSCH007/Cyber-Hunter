69792b2504055921dcc4e902292289687c0f42d7
Waffelhunter <Waffelhunter@protonmail.com>
2022-10-09 18:49:16 +0200

    Cyber blade rotation fixed

---
+++ Cyber-Hunt/Assets/Scripts/BladeRotator.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeRotator : MonoBehaviour
+    
+{
+    [SerializeField] private Vector3 _rotation;
+    public void Update()
+    {
+        transform.Rotate(_rotation * Time.deltaTime);
+    }
+
+
+}
