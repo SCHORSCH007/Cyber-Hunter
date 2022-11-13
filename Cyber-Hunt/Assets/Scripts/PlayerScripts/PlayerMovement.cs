c0a494b65fbbabf61f43633854a1a2742368b685
Waffelhunter <Waffelhunter@protonmail.com>
2022-11-13 18:30:47 +0100

    Help pls

--- Cyber-Hunt/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Cyber-Hunt/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -29,28 +29,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+       
         //input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.deltaTime);
-
-        if(movement.y < 0)
-        {
-            player.eulerAngles = new Vector3(0, -180, 0);
-        }
-        else
-        {
-            player.eulerAngles = new Vector3(0, 180, 0);
-        }
-       
+        
     }
     private void FixedUpdate()
     {
         //movement
         
-        if(_dashVelocity > 0)
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        if (_dashVelocity > 0)
         {
             rb.AddForce(rb.position + movement.normalized * _dashVelocity * Time.fixedDeltaTime);
             //_dashVelocity = 0f;
